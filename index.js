6623ca46e7d74d7fcb09af7a2b1c8cdde75734f3
Olivia McKee <olivia.mckee97@gmail.com>
2022-07-02 20:13:18 -0500

    generate full html with cards

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+// import required modules
 const fs = require('fs');
 const inquirer = require("inquirer");
 const Employee = require("./lib/Employee.js");
 const Engineer = require("./lib/Engineer.js");
 const Intern = require("./lib/Intern.js");
 const Manager = require("./lib/Manager.js");
+
+//create empty arrays for each type of employee
+const managerArr = [];
+const engineerArr = [];
+const internArr = [];
 
 const managerQuestions = [
     {
@@ -83,11 +89,15 @@ const menuQuestion = [
     }
 ];
 
+const init = () => {
+    promptManager();
+}
+
 const promptManager = () => {
     return inquirer
     .prompt(managerQuestions)
     .then((answers) => {
-        new Manager(answers);
+        managerArr.push(new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOffice));
         return promptMenu();
     })
     .catch(err => {
@@ -106,10 +116,22 @@ const promptMenu = () => {
             return promptIntern();
         };
         if (answers.menu == "Done") {
-            writetoFile();
-            copyStyleSheet();
+            console.log(managerArr);
+            console.log(engineerArr);
+            console.log(internArr);
             return;
         };
+    })
+    .then(generateHtml)
+    .then(pageData => {
+        return writetoFile(pageData);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyStyleSheet();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
     })
     .catch(err => {
         console.log(err);
@@ -120,7 +142,7 @@ const promptEngineer = () => {
     return inquirer
     .prompt(engineerQuestions)
     .then((answers) => {
-        new Engineer(answers);
+        engineerArr.push(new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub));
         return promptMenu();
     })
     .catch(err => {
@@ -132,12 +154,84 @@ const promptIntern = () => {
     return inquirer
     .prompt(internQuestions)
     .then((answers) => {
-        new Intern(answers);
+        internArr.push(new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool));
         return promptMenu();
     })
     .catch(err => {
         console.log(err);
     });
+};
+
+const generateHtml = () => {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./style.css">
+        <title>Team Profile</title>
+    </head>
+    <body>
+        <header>My Team</header>
+        <section>
+            <div class="card">
+                <h1 class="name">${managerArr[0].name}</h1>
+                <h2 class="role">Manager</h2>
+                <div class="details">
+                    <p>ID: ${managerArr[0].id}</p>
+                    <p>Email: <a href = "mailto: ${managerArr[0].email}">${managerArr[0].email}</a></p>
+                    <p>Office number: ${managerArr[0].officeNumber}</p>
+                </div>
+            </div>
+
+        ${engineerCard()}
+        ${internCard()}
+
+        </section>
+    </body>
+    </html>
+    `;
+};
+
+
+const engineerCard = engineer => {
+    let engineers = "";
+    engineerArr.forEach((Engineer) => {
+        let engineer = `
+        <div class="card">
+                    <h1 class="name">${Engineer.name}</h1>
+                    <h2 class="role">Engineer</h2>
+                    <div class="details">
+                        <p>ID: ${Engineer.id}</p>
+                        <p>Email: <a href = "mailto: ${Engineer.email}">${Engineer.email}</a></p>
+                        <p>Github: <a href = "http://github.com/${Engineer.github}">${Engineer.github}</a></p>
+                    </div>
+                </div>
+        `
+    engineers += engineer;
+    })
+    return engineers;
+};
+
+const internCard = intern => {
+    let interns = "";
+    internArr.forEach((Intern) => {
+        let intern = `
+        <div class="card">
+                    <h1 class="name">${Intern.name}</h1>
+                    <h2 class="role">Intern</h2>
+                    <div class="details">
+                        <p>ID: ${Intern.id}</p>
+                        <p>Email: <a href = "mailto: ${Intern.email}">${Intern.email}</a></p>
+                        <p>School: ${Intern.school}</p>
+                    </div>
+                </div>
+        `
+    interns += intern;
+    })
+    return interns;
 };
 
 function writetoFile(data) {
@@ -155,78 +249,13 @@ function writetoFile(data) {
     });
 };
 
-const generateHtml = (data) => {
-    return `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="./style.css">
-        <title>Team Profile</title>
-    </head>
-    <body>
-        <header>My Team</header>
-        <section>
-            ${managerCard()}
-            ${engineerCard()}
-            ${internCard()}
-        </section>
-    </body>
-    </html>
-    `;
-};
-
-const managerCard = manager => {
-    return `
-    <div class="card">
-                <h1 class="name">${manager.name}</h1>
-                <h2 class="role">Manager</h2>
-                <div class="details">
-                    <p>ID: ${manager.id}</p>
-                    <p>Email: ${manager.email}</p>
-                    <p>Office number: ${manager.officeNumber}</p>
-                </div>
-            </div>
-    `
-}
-
-const engineerCard = engineer => {
-    return `
-    <div class="card">
-                <h1 class="name">${engineer.name}</h1>
-                <h2 class="role">Manager</h2>
-                <div class="details">
-                    <p>ID: ${engineer.id}</p>
-                    <p>Email: ${engineer.email}</p>
-                    <p>Github: ${engineer.github}</p>
-                </div>
-            </div>
-    `
-}
-
-const internCard = intern => {
-    return `
-    <div class="card">
-                <h1 class="name">${intern.name}</h1>
-                <h2 class="role">Manager</h2>
-                <div class="details">
-                    <p>ID: ${intern.id}</p>
-                    <p>Email: ${intern.email}</p>
-                    <p>School: ${intern.school}</p>
-                </div>
-            </div>
-    `
-}
-
-const copyStyleSheet = () => {
+function copyStyleSheet() {
     return new Promise((resolve, reject) => {
-        fs.copyFile('./style.css', '../dist/style.css', err => {
+        fs.copyFile('src/style.css', './dist/style.css', err => {
             if (err) {
+                reject(err);
                 return;
             }
-
             resolve({
                 ok:true,
                 message: 'Stylesheet created!'
@@ -235,4 +264,4 @@ const copyStyleSheet = () => {
     });
 };
 
-promptManager();
+init();
